bdb53858f3bf84d15dfd97fb60cfbae2de836e68
Bret Farley <bretfarley12@mittymonarchl.com>
2017-10-17 12:25:22 -0700

    init commit

---
+++ apps/time_display/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse, redirect
+from time import gmtime, strftime, localtime
+def index(request):
+
+    context = {
+
+        "date": strftime("%Y-%m-%d", localtime()),
+        "time": strftime("%H:%M %p", localtime())
+    }
+    return render(request,'time_display/index.html', context)
+
